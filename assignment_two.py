2484480c9db08067bfd95ac094fd93fcd2dc18de
ritesh <gettoriteshsingh@gmail.com>
2018-06-06 01:39:13 +0530

    sec assignment

---
+++ assignment_two.py
@@ -0,0 +1,25 @@
+#Print anything you want on screen
+print("Learning with AcadView is great")
+
+#Join two strings using '+' 
+print('Acad'+'View makes learning a fun')
+
+#Take the input of 3 variables x,y and z.Print their values on screen
+x=input("enter x:")
+y=input("enter y:")
+z=input("enter z:")
+print(x,y,z,sep="\n")
+
+#print let's get started on the screen.
+print("Let\'s get started")
+
+#print the following values using placeholders
+s="Acadview"
+course="Python"
+fees=5800
+print(("%s course for %s is of %d rupees")%(s,course,fees))
+
+#Something interesting
+name="Tony Stark"
+salary=1000000
+print(('%s earns %d')%(name,salary)) 
