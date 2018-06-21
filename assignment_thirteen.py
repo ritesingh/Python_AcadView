c4060b3fd84c3f38dfa64b5d04bc66783499bc42
Ritesh SiNgh <gettoriteshsingh@gmail.com>
2018-06-21 15:00:31 +0530

    exception handling

--- assignment_thirteen.py
+++ assignment_thirteen.py
@@ -27,9 +27,10 @@ try:
 except IndexError:
     print("jitna kambal hai utna pair failaao!!")
 
+
 """Q.3- What will be the output of the following code:
 
-# Program to depict Raising Exception
+Program to depict Raising Exception
  
 try:
     raise NameError("Hi there")  # Raise Error
@@ -37,10 +38,77 @@ except NameError:
     print "An exception"
     raise  # To determine whether the exception was raised or not
 """
-try:
-    raise NameError("hello")  # Raise Error displaying Hi there
+print("OUTPUT WILL BE ->An exception")
 
-except NameError:
-    print("An exception")
+
+
+"""
+Q.4- What will be the output of the following code:
+
+ # Function which returns a/b
+def AbyB(a , b):
+	try:
+		c = ((a+b) / (a-b))
+	except ZeroDivisionError:
+		print "a/b result in 0"
+	else:
+		print c
+
+# Driver program to test above function
+AbyB(2.0, 3.0)
+AbyB(3.0, 3.0)"""
+
+print("-5.0\na/b result in 0")
+print("when a=2.0 and b=3.0 their difference is not zero and thus they execute normally which is not the case with a=3.0 and b =3.0")
+
+
+"""
+Q.5- Write a program to show and handle following exceptions: 
+1. Import Error
+2. Value Error
+3. Index Error
+"""
+try:
+    import lapata
+
+except ImportError:                             
+    print("Import/export ka business hai kya? ye python hai!")     
+else:
+    print("u r smart !")
+try:
+    y = int(input("Enter number:"))
+    
+except ValueError:
+    print("padh to lete manga kya tha.. ValueError jhelo ab")
+
+else:
+    print("Cool u r smart!")
+
+try:
+    arr=[1,2,3,4]
+    print(arr[5])
+
+except IndexError:
+    print("Kaha na kambal dekh k pair failaao! Jhelo IndexError")
+
+
+"""
+Q.6- Create a user-defined exception AgeTooSmallError() that warns the user when they have entered age less than 18. 
+The code must keep taking input till the user enters the appropriate age number(less than 18). """
+class AgeTooSmallError(Exception):          #class for the custom error
+    print("naabaaliko k liye khed hai!")
+def trying():
+    try:
+        age = int(input("Enter age:"))
+        if age<18:
+            raise AgeTooSmallError              #raises the error we defined
+
+    except AgeTooSmallError:
+        if age<18 :
+            print("ERROR, enter age above 18 ") #keep entering until done        
+            trying()
+    else:
+        print("now you can proceed!!")
+trying()
 
 
